d478ecf87f72baf3a2f82c4f8a4243a01b3d5c8a
mirkomikac <mmikac27@gmail.com>
2017-12-11 19:49:41 +0100

    Initial commit

---
+++ ExternalRequestHandlerService/src/main/java/com/sep/tim2/erh/ExternalRequestHandlerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.sep.tim2.erh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalRequestHandlerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalRequestHandlerServiceApplication.class, args);
+	}
+}
---
+++ ExternalRequestHandlerService/src/test/java/com/sep/tim2/erh/ExternalRequestHandlerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sep.tim2.erh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExternalRequestHandlerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
