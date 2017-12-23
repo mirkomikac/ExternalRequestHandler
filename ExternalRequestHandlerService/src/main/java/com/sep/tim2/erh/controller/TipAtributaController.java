74e004d8e687ed81b5d6ac7df0ba2e63d792a8d0
darkotacic <darko.tacic@outlook.com>
2017-12-23 17:15:03 +0100

    Added controller entity TipAtributa

---
+++ ExternalRequestHandlerService/src/main/java/com/sep/tim2/erh/controller/TipAtributaController.java
@@ -0,0 +1,35 @@
+package com.sep.tim2.erh.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.sep.tim2.erh.config.DatabaseUri;
+import com.sep.tim2.erh.model.TipAtributa;
+
+@RestController
+@RequestMapping("/tipoviAtributa")
+public class TipAtributaController {
+	
+	private final RestTemplate restTemplate;
+	private final DatabaseUri databaseUri;
+	
+	@Autowired
+	public TipAtributaController(RestTemplate restTemplate, DatabaseUri databaseUri) {
+		this.restTemplate = restTemplate;
+		this.databaseUri = databaseUri;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@GetMapping("zaTipOsiguranja/{tipOsiguranjaId}")
+	@ResponseBody
+	public List<TipAtributa> getTipoviAtributa(@PathVariable("tipOsiguranjaId")Long tipOsiguranjaId) {
+		return restTemplate.getForObject(databaseUri.getDatabaseUri() + "/tipoviAtributa/zaTipOsiguranja/" + tipOsiguranjaId, List.class);
+	}
+}
